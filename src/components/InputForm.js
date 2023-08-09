80fc07666055aa8c933af19d2bb5e2ba76bd13a9
erikbocks <erik.bock@outlook.com>
2023-08-09 13:26:21 -0300

    troca do handler do input e refactor no formulario de login

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -7,7 +7,7 @@ function FormInput(props) {
     return (
         <div className={'w-screen h-20 flex flex-col  items-center justify-evenly'}>
             <h3 className={'text-xl xl:text-lg'}>{title}</h3>
-            <input className={'p-2 w-60 border border-solid rounded-lg border-slate-500 bg-gray-100'} type={type} placeholder={placeholder} name={name} onChange={props.handleSignupFormData} required></input>
+            <input className={'p-2 w-60 border border-solid rounded-lg border-slate-500 bg-gray-100'} type={type} placeholder={placeholder} name={name} onChange={props.handleInputChange} required></input>
         </div>
     )
 }
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,11 +1,29 @@
 import { React, useState } from 'react'
 import { Link } from "react-router-dom"
+import FormInput from './InputForm'
 
 function LoginForm(props) {
     const [loginFormData, setFormData] = useState({
         login: "",
         password: ""
     })
+
+    const inputs = [
+        {
+            id: 1,
+            title: "Email ou Usuário",
+            type: "text",
+            placeholder: "Email ou Usuário",
+            name: "login"
+        },
+        {
+            id: 2,
+            title: "Senha",
+            type: "password",
+            placeholder: "Senha",
+            name: "password"
+        }
+    ]
 
     const handleInputChange = (event) => {
         const fieldName = event.target.getAttribute('name')
@@ -30,14 +48,9 @@ function LoginForm(props) {
         <div className={'h-2/4 flex flex-col items-center'}>
             <div className={'bg-white rounded-3xl flex justify-evenly flex-col w-5/6 md:max-lg:w-3/6 md:max-lg:h-3/4 xl:w-1/5 h-full items-center border-2 drop-shadow-md '}>
                 <form className={'h-4/6 w-1/5 flex flex-col justify-between items-center'} onSubmit={handleSubmit}>
-                    <div className={'w-screen h-20 flex flex-col items-center justify-evenly'}>
-                        <h3 className={'text-xl xl:text-lg'}>Email ou Usuário</h3>
-                        <input className={"p-2 w-60 border border-solid rounded-lg border-slate-500 bg-gray-100"} type={"text"} placeholder={"Email ou Usuário"} name={"login"} onChange={handleInputChange} required></input>
-                    </div>
-                    <div className={'w-screen h-20 flex flex-col items-center justify-evenly'}>
-                        <p className={'text-xl xl:text-lg'}>Senha</p>
-                        <input className={"p-2 w-60 border border-solid rounded-lg border-slate-500 bg-gray-100"} type={"password"} placeholder={"Senha"} name={"password"} onChange={handleInputChange} required></input>
-                    </div>
+                    {inputs.map((input) => {
+                        return <FormInput key={input.id} {...input} handleInputChange={handleInputChange} />
+                    })}
                     <div className={'w-32 h-20 flex justify-center items-center'}>
                         <button type={"submit"} className={"h-12 w-24 rounded-full hover:scale-105 transition-all text-white bg-sky-600 hover:bg-blue-500"}>Entrar</button>
                     </div>
